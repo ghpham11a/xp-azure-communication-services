43986fba3609e22f2eaf1c3479ea1415cd551668
Anthony Pham <gm.pham@gmail.com>
2026-01-18 09:50:55 -0500

    fix react version

--- react-client/vite.config.ts
+++ react-client/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// plugins: [['babel-plugin-react-compiler']],
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [],
       },
     }),
   ],
